77c3788940ecc1b18d31c47cac02b690b7c9835c
Mem0Real <juniourmimo@gmail.com>
2024-10-12 20:19:25 +0300

    Say hello design completed, going to work on footer

--- src/app/mainPages/home_components/SayHello.jsx
+++ src/app/mainPages/home_components/SayHello.jsx
@@ -1,65 +1,94 @@
 "use client";
 
 import React from "react";
-import { useForm } from "@mantine/form";
-import { Textarea, TextInput, Button } from "@mantine/core";
+
+import { motion } from "framer-motion";
 
 export default function SayHello() {
-	const form = useForm({
-		mode: "uncontrolled",
-		initialValues: { name: "", email: "", message: "" },
-
-		// functions will be used to validate values at corresponding key
-		validate: {
-			name: (value) =>
-				value.length < 2 ? "Name must have at least 2 letters" : null,
-			email: (value) => (/^\S+@\S+$/.test(value) ? null : "Invalid email"),
-			message: (value) =>
-				value.length < 2 ? "Message must have at least 2 letters" : null,
-		},
-	});
-
 	return (
-		<div className="w-full px-12 xl:px-40">
-			<div className="h-[60vh] flex justify-start items-center">
-				<div className="w-[80%] mx-auto flex justify-start items-start gap-8">
-					<div className="w-full flex flex-col justify-start items-start">
-						<h1 className="text-5xl text-neutral-900 capitalize">Say Hello!</h1>
-						<form
-							className="h-56 w-full border border-black px-4 mt-5 flex flex-col gap-4 justify-center items-center"
+		<div className="w-full px-2 md:px-4 xl:px-40 py-6 pb-8 md:py-12 xl:py-2">
+			<div className="xl:h-[70vh] flex flex-col xl:flex-row justify-start items-center">
+				<div className="w-full md:max-w-[50em] xl:max-w-[80em] mx-auto flex flex-col xl:flex-row  justify-start items-start gap-2 md:gap-4 xl:gap-8">
+					<div className="w-full flex flex-col justify-start items-start gap-1 xl:gap-5">
+						<h1 className="text-5xl text-neutral-900 capitalize ms-2 xl:ms-5">
+							Say Hello!
+						</h1>
+						{/* <form
+							className="h-full w-full px-2 mt-5 flex flex-col gap-4 justify-center items-center"
 							onSubmit={form.onSubmit(console.log("Sub"))}
 						>
-							<div className="flex gap-8">
+							<div className="flex gap-4 justify-center items-start">
 								<TextInput
 									variant="filled"
-									label="Name"
 									placeholder="Name"
 									key={form.key("name")}
 									{...form.getInputProps("name")}
+									aria-label="name"
 								/>
 								<TextInput
 									variant="filled"
-									mt="sm"
-									label="Email"
 									placeholder="Email"
 									key={form.key("email")}
 									{...form.getInputProps("email")}
+									aria-label="email"
 								/>
 							</div>
-							<Textarea
-								variant="filled"
-								radius="md"
-								placeholder="Input placeholder"
-								key={form.key("message")}
-								{...form.getInputProps("message")}
-							/>
+							<div className="w-full">
+								<Textarea
+									variant="filled"
+									size="sm"
+									radius="md"
+									placeholder="Message"
+									key={form.key("message")}
+									{...form.getInputProps("message")}
+									aria-label="message"
+									// inputSize="xl"
+									style={{
+										"--radius": "0rem",
+										borderRadius: "var(--radius)",
+									}}
+								/>
+							</div>
 							<Button type="submit" mt="sm">
 								Submit
 							</Button>
+						</form> */}
+						<form
+							action=""
+							className="h-full w-full px-2 mt-5 flex flex-col gap-4 justify-center items-center"
+						>
+							<div className="w-full flex flex-col sm:flex-row items-start gap-5">
+								<input
+									className="basis-1/2 shrink py-2 bg-neutral-200/60 rounded px-2"
+									placeholder="Name"
+								/>
+								<input
+									className="basis-1/2 shrink py-2 bg-neutral-200/60 rounded px-2"
+									placeholder="Email"
+								/>
+							</div>
+							<div className="w-full">
+								<textarea
+									className="w-full min-h-36 bg-neutral-200/60 rounded px-2"
+									placeholder="Message"
+								/>
+							</div>
+
+							<div className="self-start">
+								<motion.h1
+									whileHover={{
+										backgroundColor: "#DFA70C",
+										color: "#FFF",
+									}}
+									className="px-6 py-2 border border-mellow text-mellow font-bold cursor-pointer capitalize"
+								>
+									Send _
+								</motion.h1>
+							</div>
 						</form>
 					</div>
-					<div className="w-full flex flex-col justify-center items-start gap-4">
-						<div className="invisible flex-1/4 h-8" />
+					<div className="w-full flex flex-col md:flex-row md:flex-wrap xl:flex-col justify-center items-start gap-4">
+						<div className="invisible h-8" />
 						<div className="flex flex-col gap-2 pr-8">
 							<h1 className="text-2xl font-semibold text-neutral-900 capitalize">
 								Address
