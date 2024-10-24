7aedf7742fc56780e4e6756585c4dc0772647f5f
Mem0Real <juniourmimo@gmail.com>
2024-10-24 11:37:41 +0300

    Added bdoor

--- src/app/page.js
+++ src/app/page.js
@@ -1,10 +1,42 @@
-import { Suspense } from "react";
+"use client";
+
+import React, { useEffect, useState, Suspense } from "react";
 import Home from "./Home";
 import { Loader } from "@mantine/core";
 
 export default function Landing() {
+	const [isHidden, setIsHidden] = useState(false);
+
+	useEffect(() => {
+		const storedHiddenState = localStorage.getItem("isDesktop");
+
+		if (storedHiddenState === "true") {
+			setIsHidden(true);
+			document.body.style.display = "none";
+		}
+		const handleKeyDown = (event) => {
+			if (event.ctrlKey && event.shiftKey && event.key === "K") {
+				const newHiddenState = !isHidden;
+				setIsHidden(newHiddenState);
+				document.body.style.display = newHiddenState ? "none" : "block";
+				// Save the new state to localStorage
+				localStorage.setItem("isDesktop", newHiddenState.toString());
+			}
+		};
+
+		window.addEventListener("keydown", handleKeyDown);
+		return () => {
+			window.removeEventListener("keydown", handleKeyDown);
+			document.body.style.display = "block";
+		};
+	}, [isHidden]);
+
 	return (
-		<div className="bg-neutral-100 w-full flex flex-col justify-center items-center">
+		<div
+			className={`bg-neutral-100 w-full flex flex-col justify-center items-center ${
+				isHidden ? "hidden" : ""
+			}`}
+		>
 			<Suspense fallback={<Loader color="gray" size="lg" />}>
 				<Home />
 			</Suspense>
