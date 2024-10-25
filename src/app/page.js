877b8c9fa511e323083f845c575871f25578a6ff
Mem0Real <juniourmimo@gmail.com>
2024-10-25 16:43:52 +0300

    Removed 'use client' from page.js && git push

--- src/app/page.js
+++ src/app/page.js
@@ -1,36 +1,8 @@
-"use client";
-
-import React, { useEffect, useState, Suspense } from "react";
+import React, { Suspense } from "react";
 import Home from "./Home";
 import { Loader } from "@mantine/core";
 
 export default function Landing() {
-	// const [isHidden, setIsHidden] = useState(false);
-
-	// useEffect(() => {
-	// 	const storedHiddenState = localStorage.getItem("isDesktop");
-
-	// 	if (storedHiddenState === "true") {
-	// 		setIsHidden(true);
-	// 		document.body.style.display = "none";
-	// 	}
-	// 	const handleKeyDown = (event) => {
-	// 		if (event.ctrlKey && event.shiftKey && event.key === "K") {
-	// 			const newHiddenState = !isHidden;
-	// 			setIsHidden(newHiddenState);
-	// 			document.body.style.display = newHiddenState ? "none" : "block";
-	// 			// Save the new state to localStorage
-	// 			localStorage.setItem("isDesktop", newHiddenState.toString());
-	// 		}
-	// 	};
-
-	// 	window.addEventListener("keydown", handleKeyDown);
-	// 	return () => {
-	// 		window.removeEventListener("keydown", handleKeyDown);
-	// 		document.body.style.display = "block";
-	// 	};
-	// }, [isHidden]);
-
 	return (
 		<div
 			className={`bg-neutral-100 w-full flex flex-col justify-center items-center`}
